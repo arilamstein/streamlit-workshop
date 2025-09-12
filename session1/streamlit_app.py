483fcdf2011a19228a7028b33d13cb6097da2d56
Ari Lamstein <arilamstein@gmail.com>
2025-09-12 12:41:34 -0700

    update to session1 slides

--- session1/streamlit_app.py
+++ session1/streamlit_app.py
@@ -1,21 +1,10 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
+st.title("Demo Streamlit App")
 
 df = pd.read_csv("state_data.csv")
 
-st.header("US State Demographics")
-
-# Let user select which state to graph
-state = st.selectbox("State:", df["State"].unique())
-
-# Create a graph of total population
-df_state = df[df["State"] == state]
-fig = px.line(
-    df_state, x="Year", y="Total Population", title=f"Total Population of {state}"
-)
-st.plotly_chart(fig)
-
-# Show the entire dataframe
-st.write("All Data")
 st.dataframe(df)
+
+
