50f854f7ae4dc41629c7931d76d9b691e8c96d64
Ari Lamstein <arilamstein@gmail.com>
2025-09-14 13:58:48 -0700

    Add material for user input.

---
+++ session2/input_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Demo Streamlit App")
+
+df = pd.read_csv("state_data.csv")
+
+# Exercise: Change this code to:
+# 1. Ask the user to select a state
+# 2. Have it populate with all the list of unique states
+option = st.selectbox(
+    'Choose your favorite fruit:',
+    ['Apple', 'Banana', 'Cherry']
+)
+
+st.write('You selected:', option)
+
+st.dataframe(df)
