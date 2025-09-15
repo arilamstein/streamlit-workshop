de8763600c292811441f1132e6ba19ff092a0a9b
Ari Lamstein <arilamstein@gmail.com>
2025-09-15 16:13:59 -0700

    update for graphs

---
+++ session2/graph_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title("Demo Streamlit App")
+
+df = pd.read_csv("state_data.csv")
+
+state = st.selectbox("Select a State:", df['State'].unique())
+
+mask = df['State'] == state
+df_state = df[mask]
+
+st.dataframe(df_state)
