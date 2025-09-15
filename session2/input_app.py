37fa6800230afd8167520c69cd95db734c966fa6
Ari Lamstein <arilamstein@gmail.com>
2025-09-15 10:28:18 -0700

    Update app to not have solution

--- session2/input_app.py
+++ session2/input_app.py
@@ -9,8 +9,8 @@ df = pd.read_csv("state_data.csv")
 # 1. Ask the user to select a state
 # 2. Have it populate with all the list of unique states
 option = st.selectbox(
-    'Select a State:',
-    df['State'].unique()
+    'Select your favorite fruit:',
+    ['Apple', 'Banana', 'Cherry']
 )
 
 st.write('You selected:', option)
