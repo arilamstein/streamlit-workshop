df983a57894c691fc946905375ff7c899e62224e
Ari Lamstein <arilamstein@gmail.com>
2026-07-16 16:12:07 -0700

    Clean up final-app/final_app.py
    -Remove final-app/backend.py as course no longer teaches about backend module.
    -Remove final-app/state_data.csv - this is a duplicate from /state_data.csv.
    -Update final-app/final_app.py to use Path module when loading data
    
    Initially this app loaded the data with "../state_data.csv". This worked locally
    (because I ran the app within /final-app with "streamlit run final_app.py"),
    but not in production (where the app is presumably run from / with something
    like "streamlit run final-app/final_app.py").
    
    In Python, at run time relative paths like "../" are resolved based on the directory
    you're invoking python from, not where the file is located on disk. Using the
    path module like:
    
    DATA_PATH = Path(__file__).parent.parent / "state_data.csv"
    
    makes the load work regardless of whatever directory you're in when you
    run the app.

--- final-app/final_app.py
+++ final-app/final_app.py
@@ -1,25 +1,43 @@
 import streamlit as st
-import backend as be
+import pandas as pd
+import plotly.express as px
+from pathlib import Path
+import pandas as pd
 
 st.title("US State Demographics")
 
+DATA_PATH = Path(__file__).parent.parent / "state_data.csv"
+df = pd.read_csv(DATA_PATH)
+
 col1, col2, col3 = st.columns(3)
 with col1:
-    state = st.selectbox("State:", be.get_unique_states())
+    state = st.selectbox("State:", df["State"].unique())
 with col2:
     demographic = st.selectbox(
         "Demographic:", ["Total Population", "Median Household Income"]
     )
 with col3:
-    year = st.selectbox("Year:", be.get_unique_years())
+    year = st.selectbox("Year:", df["Year"].unique())
 
 graph_tab, map_tab, table_tab = st.tabs(["📈 Graph", "🗺️ Map", "📊 Table"])
 with graph_tab:
-    fig = be.get_line_graph(state, demographic)
+    mask = df["State"] == state
+    df_state = df[mask]
+    fig = px.line(df_state, x="Year", y=demographic, title=f"{demographic} for {state}")
     st.plotly_chart(fig)
 with map_tab:
-    fig = be.get_map(demographic, year)
+    mask = df["Year"] == year
+    df_year = df[mask]
+
+    fig = px.choropleth(
+        df_year,
+        locations="State Abbrev",  # Column for region
+        locationmode="USA-states",
+        color=demographic,  # Column for color
+        scope="usa",
+        title=f"{demographic} for {year}",
+        color_continuous_scale="viridis",
+    )
     st.plotly_chart(fig)
 with table_tab:
-    df = be.get_data()
-    st.dataframe(df)
+    st.dataframe(df_state)
